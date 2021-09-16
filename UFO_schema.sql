79650c783e079da7ad406ae160b7140a72f5fb82
byronpineda225 <84112237+byronpineda225@users.noreply.github.com>
2021-09-15 22:57:05 -0400

    Added UFO Schema and checks sql

---
+++ UFO_schema.sql
@@ -0,0 +1,46 @@
+-- Please note the database is named UFO_Project_db
+-- DROP TABLE "ufo_sightings";
+
+CREATE TABLE "ufo_sightings" (
+  --  id SERIAL,
+    "datetime" VARCHAR(20),
+    "city" VARCHAR(100),
+    "state" VARCHAR(2),
+    "country" VARCHAR(2),
+    "shape" VARCHAR(20),
+    "duration (seconds)" NUMERIC,
+    "comments" VARCHAR(250),   
+    "latitude" NUMERIC,   
+    "longitude" NUMERIC(15)
+ --	PRIMARY KEY(id)
+);
+/* Perform a few checks
+SELECT * FROM ufo_sightings;
+
+SELECT DISTINCT(country)
+FROM ufo_sightings
+ORDER BY 1 DESC;
+
+-- Countries and number of sightings
+SELECT DISTINCT(country), COUNT(country)
+FROM ufo_sightings
+GROUP BY country
+ORDER BY COUNT(country) DESC;			   
+
+-- Get the top 5 states with most sightings
+SELECT UPPER(state), COUNT(state)
+FROM ufo_sightings
+GROUP BY state
+ORDER BY COUNT(state) DESC
+FETCH FIRST 5 ROWS ONLY;
+
+-- Find some invalid dates
+SELECT datetime 
+FROM ufo_sightings
+WHERE datetime NOT LIKE '%/%';
+
+SELECT state, ROUND(SUM("duration (seconds)"),0)
+FROM ufo_sightings
+GROUP BY state
+ORDER BY SUM("duration (seconds)") DESC;
+*/
