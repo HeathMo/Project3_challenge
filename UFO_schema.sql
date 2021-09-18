6cd98cc3fd6684eb8225e3de0a09b2b1bdebcf8f
byronpineda225 <84112237+byronpineda225@users.noreply.github.com>
2021-09-18 15:29:36 -0400

    Added in updated database and ERD diagram

--- UFO_schema.sql
+++ UFO_schema.sql
@@ -1,9 +1,9 @@
 -- Please note the database is named UFO_Project_db
+
 -- DROP TABLE "ufo_sightings";
 
 CREATE TABLE "ufo_sightings" (
-  --  id SERIAL,
-    "datetime" VARCHAR(20),
+    "datetime" DATE,
     "city" VARCHAR(100),
     "state" VARCHAR(2),
     "country" VARCHAR(2),
@@ -11,13 +11,16 @@ CREATE TABLE "ufo_sightings" (
     "duration (seconds)" NUMERIC,
     "comments" VARCHAR(250),   
     "latitude" NUMERIC,   
-    "longitude" NUMERIC(15)
- --	PRIMARY KEY(id)
+    "longitude" NUMERIC
 );
+
 /* Perform a few checks
-SELECT * FROM ufo_sightings;
 
 SELECT DISTINCT(country)
+FROM ufo_sightings
+ORDER BY 1 DESC;
+
+SELECT DISTINCT(state)
 FROM ufo_sightings
 ORDER BY 1 DESC;
 
@@ -27,29 +30,20 @@ FROM ufo_sightings
 GROUP BY country
 ORDER BY COUNT(country) DESC;			   
 
--- Get the top 5 states with most sightings
+-- Get the top 20 states with most sightings in the US
 SELECT UPPER(state), COUNT(state)
 FROM ufo_sightings
+WHERE country = 'us'
 GROUP BY state
 ORDER BY COUNT(state) DESC
-FETCH FIRST 5 ROWS ONLY;
+FETCH FIRST 20 ROWS ONLY;
 
--- Find some invalid dates
-SELECT datetime 
+-- Get the top 20 states with most sightings not in the US
+SELECT UPPER(state), COUNT(state)
 FROM ufo_sightings
-WHERE datetime NOT LIKE '%/%';
-
-SELECT state, ROUND(SUM("duration (seconds)"),0)
-FROM ufo_sightings
+WHERE country != 'us'
 GROUP BY state
-ORDER BY SUM("duration (seconds)") DESC;
-
--- To get the valid dates use this type of code 
-SELECT datetime 
-FROM ufo_sightings
-WHERE datetime LIKE '%/%'
-AND TO_DATE(datetime,'MM/DD/YYYY') >= '01/01/2000';
+ORDER BY COUNT(state) DESC
+FETCH FIRST 20 ROWS ONLY;
 
 */
-
-
